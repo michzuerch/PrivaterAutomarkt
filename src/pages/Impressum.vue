3a6b6b0d093c82c21cc6402b4bfa5f0dd16f2768
Michael Zürcher <michzuerch@gmail.com>
2021-01-05 15:25:27 +0000

    Tuesday final version

--- src/pages/Impressum.vue
+++ src/pages/Impressum.vue
@@ -2,6 +2,30 @@
   <Layout>
     <h1>Impressum</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error doloremque omnis animi, eligendi magni a voluptatum, vitae, consequuntur rerum illum odit fugit assumenda rem dolores inventore iste reprehenderit maxime! Iusto.</p>
+    Impressum:
+
+Firma:           Privater Automarkt Radolfzell / WWW.BILLIG-CAR.DE
+Rechtsform:  Einzelunternehmen
+Straße:          Herrenlandstrasse 39
+Ort:              D-78315 Radolfzell
+ 
+Tel:  +49 - (0)7732 / 942407
+Fax: +49 - (0)7732 / 942408 
+E-Mail: info@billig-car.de
+ 
+Handelsregister:  Amtsgericht Radolfzell
+ 
+Handelsregister-Eintragung:  Keine Eintragung
+ 
+Umsatzsteuer-Identifikationsnr.
+nach § 27a Umsatzsteuergesetz: 
+  
+Alleininhaber: Manfred Schlett 
+
+Haftungsausschluss:
+
+Wir bemühen uns, Ihnen genaue und aktuelle Informationen zur Verfügung zu stellen. Trotz größter Sorgfalt können wir keine Gewähr für die Richtigkeit und Vollständigkeit der Inhalte dieser Internet-Seiten übernehmen. 
+
   </Layout>
 </template>
 
--- src/pages/Location.vue
+++ src/pages/Location.vue
@@ -1,23 +1,14 @@
 <template>
   <Layout>
-    <h1>Standort</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error doloremque omnis animi, eligendi magni a voluptatum, vitae, consequuntur rerum illum odit fugit assumenda rem dolores inventore iste reprehenderit maxime! Iusto.</p>
-    Anfahrt:
+    <h1>Unser Standort</h1>
+    <p>Google maps</p>
+Privater Automarkt Radolfzell<br>
+HERRENLANDSTRASSE 39<br>
+78315 RADOLFZELL am Bodensee<br>
 
-Von der Autobahn A81Abfahrt Singen folgen Sie dem Straßenverlauf durchfahren Singen Richtung Radolfzell / Konstanz.
-Nachdem Sie Böhringen durchfahren haben kommt Radolfzell. Am Ortseingang erste Kreuzung/Ampel rechts abbiegen, nun auf der Vorfahrtsstrasse bleiben nach ca. 400 mtr. sind wir mit unserem Verkaufsplatz auf der rechten Seite, gegenüber befindet sich Kawasaki und Auto Boschdienst.
-
-Von Konstanz her folgen Sie dem Straßenverlauf "Schweiz Stein am Rhein" und durchfahren Radolfzell bis zum Kreisverkehr
-Esso Tankstelle + VW-Händler. In Kreisverkehr einfahrend nehmen Sie die 3 Ausfahrt Richtung Stein am Rhein. An der nächsten Ampel biegen Sie rechts ab und nach 150 mtr. sind wir mit unserem Verkaufsplatz auf der linken Seite. Gegenüber befindet sich Auto Boschdienst und Kawasaki.
-
-Von der Höri und Überlingen her fahren Sie am Kreisverkehr in die Zeppelinstraße und dann über die Eisenbahnbrücke. An der zweiten Ampel links abbiegen. Nach 100 mtr. finden Sie uns mit unserem Verkaufsplatz auf der linken Seite. Gegenüber befindet sich Auto Boschdienst und Kawasaki.
-
-Privater Automarkt Radolfzell
-HERRENLANDSTRASSE 39
-78315 RADOLFZELL
-Bodensee
-
-
+    <div>
+        https://goo.gl/maps/FMKSLMyGqXLAzTZcA
+    </div>
   </Layout>
 </template>
 
