c50b4eae035ef8fb185f3f809cca9fa275ba7a27
Michael Zürcher <michzuerch@gmail.com>
2021-01-07 11:43:44 +0000

    12:43

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -6,7 +6,6 @@
       </strong>
       <nav class="nav">
         <g-link class="nav__link" to="/">Home</g-link>
-        <g-link class="nav__link" to="/about/">About</g-link>
         <g-link class="nav__link" to="/cars/">Unsere Autos</g-link>
         <g-link class="nav__link" to="/contact/">Kontakt</g-link>
         <g-link class="nav__link" to="/financing/">Finanzierung</g-link>
